7fbaa3987680de571444d601c17ae0064911300d
Luciano Ciccariello <xeeynamo@hotmail.com>
2020-02-05 21:14:50 +0000

    Add twitter integration

--- psncrawler/Program.cs
+++ psncrawler/Program.cs
@@ -1,4 +1,5 @@
-﻿using psncrawler.Playstation;
+﻿using Newtonsoft.Json;
+using psncrawler.Playstation;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,10 +14,12 @@ namespace psncrawler
 
         static async Task Main(string[] args)
         {
+            var configuration = await ReadConfiguration("secrets.json");
+
             var logger = SetupLogger();
             await logger.InfoAsync("Ready!");
 
-            var notifier = SetupNotifier();
+            var notifier = await SetupNotifier(configuration);
 
             if (!Directory.Exists(BasePath))
             {
@@ -27,6 +30,12 @@ namespace psncrawler
             await new Crawler(logger, notifier, BasePath, 30).ExploreMultithred(0, 30000, "np");
         }
 
+        private static async Task<Configuration> ReadConfiguration(string fileName)
+        {
+            var content = await File.ReadAllTextAsync(fileName);
+            return JsonConvert.DeserializeObject<Configuration>(content);
+        }
+
         private static ILogger SetupLogger() =>
             new Logger(new List<ILoggerHandler>()
             {
@@ -34,8 +43,25 @@ namespace psncrawler
                 new ConcurrencyLogger(new ActionLogger(LogOnFile))
             });
 
-        private static ICrawlerNotifier SetupNotifier() =>
-            new DummyCrawlerNotifier();
+        private static async Task<ICrawlerNotifier> SetupNotifier(Configuration configuration)
+        {
+            //var notifier = await TwitterCrawlerNotifier.FromConsumer(
+            //    configuration.TwitterApiKey,
+            //    configuration.TwitterApiSecret,
+            //    () =>
+            //    {
+            //        Console.Write("Twitter pin: ");
+            //        return Console.ReadLine();
+            //    });
+
+            var notifier = await TwitterCrawlerNotifier.FromAccess(
+                configuration.TwitterApiKey,
+                configuration.TwitterApiSecret,
+                configuration.TwitterToken,
+                configuration.TwitterTokenSecret);
+
+            return notifier;
+        }
 
         private static Task LogOnConsole(int severity, DateTime dateTime, string message)
         {
@@ -45,6 +71,14 @@ namespace psncrawler
 
         private static Task LogOnFile(int severity, DateTime dateTime, string message) =>
             File.AppendAllTextAsync(LogFile, $"[{dateTime.ToIso8601()}] {message}\n");
+    }
+
+    internal class Configuration
+    {
+        public string TwitterApiKey { get; set; }
+        public string TwitterApiSecret { get; set; }
+        public string TwitterToken { get; set; }
+        public string TwitterTokenSecret { get; set; }
     }
 
     internal class DummyCrawlerNotifier : ICrawlerNotifier
---
+++ psncrawler/TwitterCrawlerNotifier.cs
@@ -0,0 +1,70 @@
+﻿using psncrawler.Playstation;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using TweetSharp;
+
+namespace psncrawler
+{
+    public class TwitterCrawlerNotifier : ICrawlerNotifier
+    {
+        private readonly TwitterService _twitterService;
+
+        private TwitterCrawlerNotifier(TwitterService twitterService)
+        {
+            _twitterService = twitterService;
+        }
+
+        public Task Tweet(string message)
+        {
+            return _twitterService.SendTweetAsync(new SendTweetOptions
+            {
+                Status = message
+            });
+        }
+
+        public Task NotifyNewGameAsync(Tmdb database)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task NotifyUpdateAsync(TitlePatch patch)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static async Task<TwitterCrawlerNotifier> FromConsumer(
+            string apiKey, string apiSecret, Func<string> getVerifyCode)
+        {
+            // Pass your credentials to the service
+            var service = new TwitterService(apiKey, apiSecret);
+
+            // Step 1 - Retrieve an OAuth Request Token
+            var requestToken = await service.GetRequestTokenAsync();
+
+            // Step 2 - Redirect to the OAuth Authorization URL
+            var uri = service.GetAuthorizationUri(requestToken.Value);
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = uri.ToString(),
+                UseShellExecute = true
+            });
+
+            // Step 3 - Exchange the Request Token for an Access Token
+            var verifier = getVerifyCode();
+            var access = await service.GetAccessTokenAsync(requestToken.Value, verifier);
+
+            // Step 4 - User authenticates using the Access Token
+            service.AuthenticateWith(access.Value.Token, access.Value.TokenSecret);
+
+            return new TwitterCrawlerNotifier(service);
+        }
+        
+        public static Task<TwitterCrawlerNotifier> FromAccess(
+            string apiKey, string apiSecret, string token, string accessToken)
+        {
+            var service = new TwitterService(apiKey, apiSecret, token, accessToken);
+            return Task.FromResult(new TwitterCrawlerNotifier(service));
+        }
+    }
+}
